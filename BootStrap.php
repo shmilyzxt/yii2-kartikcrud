ea6e91b1615e56f546f9801945710cba45012a5e
shmilyzxt <49783121@qq.com>
2016-07-22 14:03:25 +0800

    create

--- BootStrap.php
+++ BootStrap.php
@@ -3,8 +3,6 @@ namespace shmilyzxt\kartikcrud;
 
 use \Yii;
 use yii\base\BootstrapInterface;
-use yii\base\Application;
-use yii\base\NotSupportedException;
 
 /**
  * bootstrap检测和设置模块，添加gii的生成器
@@ -12,14 +10,12 @@ use yii\base\NotSupportedException;
  * @since 1.0
  */
 
-class BootStrap implements BootstrapInterface
+class Bootstrap implements BootstrapInterface
 {
     public function bootstrap($app){
-        //Yii::setAlias("@kartikcrud", __DIR__);
+        Yii::setAlias("@kartikcrud", __DIR__);
         Yii::setAlias("@shmilyzxt/kartikcrud", __DIR__);
-        if(!$app->hasModule('gii')){
-            throw new NotSupportedException("该扩展需要yii2-gii支持");
-        }else{
+        if($app->hasModule('gii')){
             if (!isset($app->getModule('gii')->generators['kartikcrud'])) {
                 $app->getModule('gii')->generators['kartikcrud'] = 'shmilyzxt\kartikcrud\generators\Generator';
             }
