589b3bfdc8487245cc3a2901f60225f88fa4aa0c
shmilyzxt <49783121@qq.com>
2016-07-25 17:41:25 +0800

    modal编辑

--- assets/ajaxcrud.js
+++ assets/ajaxcrud.js
@@ -61,11 +61,4 @@ $(document).ready(function () {
             modal.open(this, selectedIds);
         }
     });
-    
-    var btn = $(".kv-btn-save");
-    if(btn){
-        btn.click(function () {
-            alert("here");return false;
-        });
-    }
 });
